4caf7ce32317a13e34aa39e8fcbf7d08578ddb61
jakubriegel <jakubriegel@hotmail.com>
2018-01-29 18:38:47 +0100

    modyfied: title subpage

--- js/timer.js
+++ js/timer.js
@@ -1,35 +1,42 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 14 2018 15:37:25" ).getTime();
+var countDownDate = new Date("Apr 14 2018 15:30:00" ).getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+var daysDiv;
+var hoursDiv;
+var minutesDiv;
+var secondsDiv;
 
-  // Get todays date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now an the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  //document.getElementById("timer").innerHTML = days + "dni " + hours + "godzin "
-  //+ minutes + "minut " + seconds + "sekund ";
-
-  document.getElementById("timer").innerHTML ="<ul> <li>" + days + "</li><li> " + hours + "</li><li> "
-  + minutes + "</li><li> " + seconds + "</li> ";
-
-  document.getElementById("timerText").innerHTML = "dni " + "godzin "
- + "minut " + "sekund ";
-
+window.onload = function(){
   
-  // If the count down is finished, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "Start";
-  }
-}, 1000);
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+    daysDiv = document.getElementById('days');
+  hoursDiv = document.getElementById('hours');
+  minutesDiv = document.getElementById('minutes');
+  secondsDiv = document.getElementById('seconds');
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    daysDiv.innerHTML = days;
+    hoursDiv.innerHTML = hours;
+    minutesDiv.innerHTML = minutes;
+    secondsDiv.innerHTML = seconds;
+    
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("timer").innerHTML = "Zapraszamy!";
+    }
+  }, 1000);
+}
