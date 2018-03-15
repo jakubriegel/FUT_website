be373b064ad247d68f68ec440b8209f3a8dde2b0
jakubriegel <jakubriegel@hotmail.com>
2018-03-15 15:37:08 +0100

    added: end time in timetable

--- js/timetable-prep.js
+++ js/timetable-prep.js
@@ -17,14 +17,14 @@ function createTimetable(){
     // function happeningNow() requires events to be sorted
     timetable.sort(function(a,b){
         // test days
-        if(a.time[0] > b.time[0]) return 1;
-        else if(a.time[0] < b.time[0]) return -1;
+        if(a.timeStart[0] > b.timeStart[0]) return 1;
+        else if(a.timeStart[0] < b.timeStart[0]) return -1;
         else{ // case days are equal test hours
-            if(a.time[1] > b.time[1]) return 1;
-            else if(a.time[1] < b.time[1]) return -1;
+            if(a.timeStart[1] > b.timeStart[1]) return 1;
+            else if(a.timeStart[1] < b.timeStart[1]) return -1;
             else{ // case hours are equal test minutes
-                if(a.time[2] > b.time[2]) return 1;
-                else if(a.time[2] < b.time[2]) return -1;
+                if(a.timeStart[2] > b.timeStart[2]) return 1;
+                else if(a.timeStart[2] < b.timeStart[2]) return -1;
                 else return 0; // case minutes are equal
             }
         }
@@ -38,7 +38,7 @@ function createTimetable(){
         if(timetable[i].extended) listItem.className = "event_extended";
         if(happeningNow(i, timeNow)) listItem.className = "event_now";
 
-        switch (timetable[i].time[0]){
+        switch (timetable[i].timeStart[0]){
             case 12:
                 insertRight(thursday, listItem, timetable[i]);
                 break;
@@ -60,34 +60,42 @@ function createTimetable(){
 }
 
 function happeningNow(i, timeNow){
-    if(timeNow.month == 3) if(timetable[i].time[0] == timeNow.day){
+    if(timeNow.month == 3) if(timetable[i].timeStart[0] == timeNow.day){
         if(i != timetable.length-1){ // case event is not last
-            if(timetable[i].time[1] == timeNow.hour){
-                if(timetable[i].time[2] <= timeNow.minutes){
-                    if(timeNow.day != timetable[i+1].time[0]) return true;
-                    else if(timeNow.hour != timetable[i+1].time[1]) return true;
-                    else if(timeNow.minutes < timetable[i+1].time[2]) return true;
+            if(timetable[i].timeStart[1] == timeNow.hour){
+                if(timetable[i].timeStart[2] <= timeNow.minutes){
+                    if(timeNow.day != timetable[i+1].timeStart[0]) return true;
+                    else if(timeNow.hour != timetable[i+1].timeStart[1]) return true;
+                    else if(timeNow.minutes < timetable[i+1].timeStart[2]) return true;
                 }
             }
-            else if(timetable[i].time[1] < timeNow.hour){
-                if(timeNow.day != timetable[i+1].time[0]) return true;
-                else if(timeNow.hour < timetable[i+1].time[1]) return true;
-                else if(timeNow.hour == timetable[i+1].time[1])
-                    if(timeNow.minutes < timetable[i+1].time[2]) return true;
+            else if(timetable[i].timeStart[1] < timeNow.hour){
+                if(timeNow.day != timetable[i+1].timeStart[0]) return true;
+                else if(timeNow.hour < timetable[i+1].timeStart[1]) return true;
+                else if(timeNow.hour == timetable[i+1].timeStart[1])
+                    if(timeNow.minutes < timetable[i+1].timeStart[2]) return true;
             }
         }
-        else if(timeNow.hour >= timetable[i].time[1]) // case event is last
-            if(timeNow.minutes >= timetable[i].time[2]) return true;
+        else if(timeNow.hour >= timetable[i].timeStart[1]) // case event is last
+            if(timeNow.minutes >= timetable[i].timeStart[2]) return true;
     }
 }
 
 function convertTime(event){
     var time = "";
-    if(event.time[1] < 10) time += '0';
-    time += event.time[1] + ':';
-    if(event.time[2] < 10) time += '0';
-    time += event.time[2];
+    if(event.timeStart[1] < 10) time += '0';
+    time += event.timeStart[1] + ':';
+    if(event.timeStart[2] < 10) time += '0';
+    time += event.timeStart[2];
 
+    if(event.timeEnd.length > 0){
+        time += "-";
+        if(event.timeEnd[0] < 10) time += '0';
+        time += event.timeEnd[0] + ':';
+        if(event.timeEnd[1] < 10) time += '0';
+        time += event.timeEnd[1];
+    }
+    
     return time;
 }
 
--- timetable.js
+++ timetable.js
@@ -2,31 +2,31 @@
     1. put comma after last event
     2. print curly braces {}
     3. input new event data inside braces in pattern:
-        "name":"EVENT_NAME", "time":[DAY, HOURS, MINUTES], "extended":true/false
+        "name":"EVENT_NAME", "timeStart":[DAY, HOURS, MINUTES], "timeEnd":[HOURS, MINUTES], "extended":true/false
     4. add new line
 */
 var timetable = [
-    {"name":"Kwaterowanie", "time":[12, 12, 0], "extended":true },
-    {"name":"Mistrzostwa FUT w Matrixa", "time":[12, 18, 0], "extended":true },
-    {"name":"Grill z SSPP", "time":[12, 21, 0], "extended":true },
-    {"name":"Śniadanie", "time":[13, 8, 0], "extended":true },
-    {"name":"Czas wolny", "time":[13, 9, 0], "extended":true },
-    {"name":"Zwiedzanie Politechniki", "time":[13, 10, 0], "extended":true },
-    {"name":"Zwiedzanie Fortów Poznańskich", "time":[13, 10, 0], "extended":true },
-    {"name":"Obiad", "time":[13, 15, 0], "extended":true },
-    {"name":"Czas wolny", "time":[13, 16, 0], "extended":true },
-    {"name":"Otwarcie sesji roboczej", "time":[13, 17, 0], "extended":false },
-    {"name":"Rada Starszych", "time":[13, 18, 0], "extended":false },
-    {"name":"Wprowadzenie do FUT", "time":[13, 18, 0], "extended":false },
-    {"name":"Kolacja", "time":[13, 20, 0], "extended":false },
-    {"name":"Integracja", "time":[13, 21, 0], "extended":false },
-    {"name":"Śniadanie", "time":[14, 8, 0], "extended":false },
-    {"name":"Panele", "time":[14, 10, 0], "extended":false },
-    {"name":"Obiad", "time":[14, 16, 0], "extended":false },
-    {"name":"Sesja Robocza", "time":[14, 17, 0], "extended":false },
-    {"name":"Kolacja", "time":[14, 20, 0], "extended":false },
-    {"name":"Niespodzianka", "time":[14, 22, 0], "extended":false },
-    {"name":"Śniadanie", "time":[15, 8, 0], "extended":false },
-    {"name":"Wykwaterowanie", "time":[15, 9, 0], "extended":false }
+    {"name":"Kwaterowanie", "timeStart":[12, 12, 0], "timeEnd":[], "extended":true },
+    {"name":"Mistrzostwa FUT w Matrixa", "timeStart":[12, 18, 0], "timeEnd":[12, 0], "extended":true },
+    {"name":"Grill z SSPP", "timeStart":[12, 21, 0], "timeEnd":[12, 0], "extended":true },
+    {"name":"Śniadanie", "timeStart":[13, 8, 0], "timeEnd":[12, 0], "extended":true },
+    {"name":"Czas wolny", "timeStart":[13, 9, 0], "timeEnd":[12, 0], "extended":true },
+    {"name":"Zwiedzanie Politechniki", "timeStart":[13, 10, 0], "timeEnd":[12, 0], "extended":true },
+    {"name":"Zwiedzanie Fortów Poznańskich", "timeStart":[13, 10, 0], "timeEnd":[12, 0], "extended":true },
+    {"name":"Obiad", "timeStart":[13, 15, 0], "timeEnd":[12, 0], "extended":true },
+    {"name":"Czas wolny", "timeStart":[13, 16, 0], "timeEnd":[12, 0], "extended":true },
+    {"name":"Otwarcie sesji roboczej", "timeStart":[13, 17, 0], "timeEnd":[12, 0], "extended":false },
+    {"name":"Rada Starszych", "timeStart":[13, 18, 0], "timeEnd":[12, 0], "extended":false },
+    {"name":"Wprowadzenie do FUT", "timeStart":[13, 18, 0], "timeEnd":[12, 0], "extended":false },
+    {"name":"Kolacja", "timeStart":[13, 20, 0], "timeEnd":[12, 0], "extended":false },
+    {"name":"Integracja", "timeStart":[13, 21, 0], "timeEnd":[12, 0], "extended":false },
+    {"name":"Śniadanie", "timeStart":[14, 8, 0], "timeEnd":[12, 0], "extended":false },
+    {"name":"Panele", "timeStart":[14, 10, 0], "timeEnd":[12, 0], "extended":false },
+    {"name":"Obiad", "timeStart":[14, 16, 0], "timeEnd":[12, 0], "extended":false },
+    {"name":"Sesja Robocza", "timeStart":[14, 17, 0], "timeEnd":[12, 0], "extended":false },
+    {"name":"Kolacja", "timeStart":[14, 20, 0], "timeEnd":[12, 0], "extended":false },
+    {"name":"Niespodzianka", "timeStart":[14, 22, 0], "timeEnd":[12, 0], "extended":false },
+    {"name":"Śniadanie", "timeStart":[15, 8, 0], "timeEnd":[12, 0], "extended":false },
+    {"name":"Wykwaterowanie", "timeStart":[15, 9, 0], "timeEnd":[12, 0], "extended":false }
 
 ];
